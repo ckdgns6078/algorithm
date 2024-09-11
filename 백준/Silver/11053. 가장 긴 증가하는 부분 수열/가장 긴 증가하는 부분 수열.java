70983308f23675734a46e5eae4d680a8f61945ba
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-09-11 16:17:41 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 248 ms, Memory: 21280 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[] arr = new long[n + 1];
+		long[] dp = new long[1500];
+		for (int i = 1; i < n + 1; i++) {
+			arr[i] = sc.nextInt();
+		}
+		dp[1] = 1;
+		long result = 1;
+
+		for (int i = 2; i < n + 1; i++) {
+			long cnt = 0;
+			for (int j = 1; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					cnt = Math.max(cnt, dp[j]);
+				}
+			}
+			dp[i] = cnt+1;
+//			System.out.println("dp [" + i + "] :" + dp[i] );
+			result = Math.max(result, dp[i]);
+		}
+		System.out.println(result);
+	}
+
+}
