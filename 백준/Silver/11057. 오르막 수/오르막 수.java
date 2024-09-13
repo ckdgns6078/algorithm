b0bab027799742c62aa8be464096191094bad067
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-09-13 19:35:16 +0900

    [Silver I] Title: 오르막 수, Time: 176 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		long[][] dp = new long[1100][11];
+		
+		for(int i = 1 ; i < 11 ;i++) {
+			dp[1][i] = 1;
+		}
+		
+		
+		for(int i = 2 ; i < n+1 ; i++) {
+			
+			for(int j=1 ; j < 11 ; j++) {
+				for(int k=1 ; k <=j ; k++) {
+					dp[i][j] = (dp[i][j] + dp[i-1][k]) % 10007;
+				}
+				
+			}
+//			System.out.println("dp[" + i + "]  "+ Arrays.toString(dp[i]));
+		}
+		
+		
+		
+		long result = 0;
+		for(int i = 1 ; i < 11 ; i++) {
+//			result = (result + dp[n][i]) % 10007;
+			result += dp[n][i];
+		}
+		result %= 10007;
+		System.out.println(result);
+		
+
+	}
+
+}
