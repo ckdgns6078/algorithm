0300c80e020b3a69bd5b48e1c0e3191b16ea52e2
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-09-30 15:37:00 +0900

    [Gold V] Title: 수 고르기, Time: 896 ms, Memory: 110940 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		long[] arr =new long[n];
+		
+		for(int i = 0 ; i < n ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		int start=0;
+		int end=0;
+		long result = Long.MAX_VALUE;
+		//시작값과 end 값중에서 뺸 값이 작을 경우 end를 증가시키고
+		//시작값과 end
+		
+		if(n==1) {
+			
+		}
+		
+		while(end < n && start < n) {
+			long min = arr[end] - arr[start];
+			if(min<m) {
+				end++;
+			}else {
+				result = Math.min(result, min);
+				start++;
+			}
+		}
+		if(result == Long.MAX_VALUE) {
+			System.out.println(arr[0]);
+		}else {
+			System.out.println(result);			
+		}
+		
+		
+	}
+	 
+}
