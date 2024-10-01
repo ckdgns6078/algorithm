dcf56262f5d825443925c9320b9e8b061d2e1432
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-10-01 15:38:35 +0900

    [Gold IV] Title: 부분합, Time: 692 ms, Memory: 109672 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int s = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int start = 0;
+		int end = 0;
+		int answer = Integer.MAX_VALUE;
+		int prefixSum =arr[start];
+		while(end < n) {
+			
+			if(prefixSum >= s) {
+				answer = Math.min(answer, end-start+1);
+			}
+			
+			if(prefixSum < s) {
+				end++;
+				if(end < n) {
+					prefixSum += arr[end];					
+				}
+			}else {
+				prefixSum-= arr[start];
+				start++;
+			}
+		}
+		
+		System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
+
+	}
+
+}
