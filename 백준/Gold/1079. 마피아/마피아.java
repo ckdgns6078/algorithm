8df350b5be1a0eda4e676286b0307d981fd1a5b7
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-10-28 14:18:57 +0900

    [Gold II] Title: 마피아, Time: 724 ms, Memory: 21180 KB -BaekjoonHub

---
+++ 백준/Gold/1079. 마피아/마피아.java
@@ -0,0 +1,179 @@
+import java.util.*;
+
+public class Main {
+
+	static int n;
+	static int[] arr;
+	static int position;
+	static int answer = 0;
+	static int[][] map;
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		arr = new int[n];
+		map = new int[n][n];
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		position = sc.nextInt();
+		
+		recursive(n , arr , 0);
+		System.out.println(answer);
+		
+	}
+	
+	public static void recursive2(int turn , int[] gameList , int result) {
+		if(mafiaCheck(gameList) || citizenCheck(gameList)) {
+			answer = Math.max(answer , result);
+//			System.out.println("종료 : " +Arrays.toString(gameList));
+			return;
+		}
+		
+		
+		if(turn%2==0) {
+			for(int i=0;i<n;i++) {
+				if(i!=position && gameList[i]!=888) {
+					int[] tempList = gameList.clone();
+					gameList[i] = 888;
+					
+					for(int j=0;j<n;j++) {
+						if(gameList[j]!=888) {
+							gameList[j] = map[i][j];
+						}
+					}
+//					System.out.println("mafia");
+					recursive(turn-1 , gameList , result +1);
+					//마피아를 살린다 , 변경한 값을 다시 원래 상태로 되돌린다.
+					for(int j=0;j<n;j++) {
+						gameList[i] = tempList[i];
+					}
+				}
+			}
+		}else {
+			int cnt =0;
+			int min = Integer.MIN_VALUE;
+			for(int i=0;i<n;i++) {
+				if(gameList[i]!=888) {
+					if(min < gameList[i]) {
+						min = Math.max(min , gameList[i]);
+						cnt = i;
+						
+						
+					}
+				}
+			}
+			int temp = gameList[cnt];
+			gameList[cnt] = 888;
+//			System.out.println("시민 : "  +Arrays.toString(gameList));
+//			System.out.println("시민이 죽인 값 : " + cnt);
+			recursive(turn -1 , gameList , result);
+			gameList[cnt] = temp; 
+		}
+		
+		
+		
+		
+	}
+
+	
+	public static void recursive(int turn, int[] gameList, int result) {
+		if (mafiaCheck(gameList) || citizenCheck(gameList)) {
+			answer = Math.max(answer, result);
+//			System.out.println("종료 : " +Arrays.toString(gameList));
+			return;
+		}
+
+		
+		if (turn % 2 == 0) {  // 밤 (은진이의 차례)
+			for (int i = 0; i < n; i++) {
+				if (gameList[i] != 888 && i != position) {
+					int originData = gameList[i];
+					mafia(i, gameList);					
+					recursive(turn - 1, gameList, result + 1);
+					reset(i , gameList , originData);
+				}
+			}
+		} else {  // 낮 (시민들의 차례)
+			int cnt =0;
+			int min = Integer.MIN_VALUE;
+			for(int i=0;i<n;i++) {
+				if(gameList[i]!=888) {
+					if(min < gameList[i]) {
+						min = Math.max(min , gameList[i]);
+						cnt = i;
+						
+						
+					}
+				}
+			}
+			int temp = gameList[cnt];
+			gameList[cnt] = 888;
+//			System.out.println("시민 : "  +Arrays.toString(gameList));
+//			System.out.println("시민이 죽인 값 : " + cnt);
+			recursive(turn -1 , gameList , result);
+			gameList[cnt] = temp; 
+		}
+	}
+
+	private static void reset(int idx, int[] gameList , int originData) {
+		for(int i=0;i<n;i++) {
+			if(gameList[i]!=888) {
+				gameList[i] -= map[idx][i];
+			}
+		}		
+		gameList[idx] = originData;
+	}
+
+	public static void mafia(int idx, int[] gameList) {
+		gameList[idx] = 888;
+		for (int i = 0; i < n; i++) {
+			if (gameList[i] != 888) {
+				gameList[i] += map[idx][i];
+			}
+		}
+	}
+	
+	public static void citizen(int[] gameList) {
+		int maxValue = Integer.MIN_VALUE;
+		int maxIndex = -1;
+		for (int i = 0; i < n; i++) {
+			if (gameList[i] != 888 && gameList[i] > maxValue) {
+				maxValue = gameList[i];
+				maxIndex = i;
+			}
+		}
+		// 유죄 지수가 가장 높은 사람 죽이기
+		if (maxIndex != -1) {
+			gameList[maxIndex] = 888;
+		}
+	}
+	
+	private static boolean mafiaCheck(int[] gameList) {		
+		if(gameList[position] == 888) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean citizenCheck(int[] gameList) {
+		int cnt =0;
+		for(int i=0;i<gameList.length;i++) {
+			if(i!=position && gameList[i]!=888) {
+				cnt++;
+			}
+		}
+		if(cnt==0) {
+			return true;
+		}else {
+			return false;
+		}		
+	}
+	
+	
+}
