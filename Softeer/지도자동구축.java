298d5b50856b76527c0016477003d5abe5f0970a
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-10-31 09:40:24 +0900

    solved 지도자동구축.java

---
+++ Softeer/지도자동구축.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int pointsCount = (int) Math.pow((Math.pow(2, N) + 1), 2);
+        System.out.println(pointsCount);   
+    }
+}
