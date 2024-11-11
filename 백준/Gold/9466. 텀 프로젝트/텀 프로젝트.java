28620c736692ccdeb414e0973257fbe6d4b9ad68
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2024-11-11 13:56:15 +0900

    [Gold III] Title: 텀 프로젝트, Time: 3868 ms, Memory: 304856 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -7,7 +7,6 @@ public class Main {
 	static boolean[] team;
 	static int answer;
 
-//	static ArrayList<Integer> list = new ArrayList();
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
