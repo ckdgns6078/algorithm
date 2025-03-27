e0522352caa93cbf39f9e4a073cdf46df9927497
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2025-03-27 09:38:26 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 96 ms, Memory: 12804 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -20,10 +20,6 @@ public class Main {
 			sb.append("\n");
 		}
 		System.out.println(sb);
-		/**
-		 * 모음(a,e,i,o,u) 하나를 반드시 포함하여야 한다. 모음이 3개 혹은 자음이 3개 연속으로 오면 안 된다. 같은 글자가 연속적으로
-		 * 두번 오면 안되나, ee 와 oo는 허용한다.
-		 */
 	}
 
 	public static boolean doubleCheck(String str) {
