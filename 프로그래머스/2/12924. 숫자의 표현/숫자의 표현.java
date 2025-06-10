dec2af3af63a7db5709e9254c6d072613b5d9ad7
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2025-06-10 15:49:50 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int solution(int n) {
+        int count = 0;
+
+        for (int k = 1; k * (k - 1) / 2 < n; k++) {
+            int numerator = n - k * (k - 1) / 2;
+            if (numerator % k == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
