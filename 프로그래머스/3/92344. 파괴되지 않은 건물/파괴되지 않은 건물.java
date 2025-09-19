ed0ecc47c428be00167c456eaec4bbfd4a495d0c
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2025-09-20 00:17:57 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 55.40 ms, Memory: 224 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        
+        int N = board.length;
+        int M = board[0].length;
+        
+        int[][] temp = new int[N+1][M+1];
+        
+        for(int[] s : skill){
+            int type = s[0];
+            int degree = s[5];
+            int r1 = s[1];
+            int c1 = s[2];
+            int r2 = s[3];
+            int c2 = s[4];
+            
+            if(type == 1){
+                degree = -degree;
+            }
+            
+            temp[r1][c1] += degree;
+            temp[r1][c2+1] -= degree;
+            temp[r2+1][c1] -= degree;
+            temp[r2+1][c2+1] += degree;
+        }
+        
+        //가로
+        for(int i =0;i<N+1;i++){
+            for(int j=1;j<M+1;j++){
+                temp[i][j] += temp[i][j-1];       
+            }
+        }
+        
+        //세로
+        for(int i =1;i<N+1;i++){
+            for(int j=0;j<M+1;j++){
+                temp[i][j] += temp[i-1][j];
+            }
+        }
+        
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(board[i][j] + temp[i][j] > 0){
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+        
+        
+        
+    }
+}
