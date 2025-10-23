99ed7b075f27c14e17c3d9046edc9f70693fcb91
ckdgns6078 <101166430+ckdgns6078@users.noreply.github.com>
2025-10-23 19:29:14 +0900

    [Platinum V] Title: 주유소, Time: 2112 ms, Memory: 219992 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.java
+++ 백준/Platinum/13308. 주유소/주유소.java
@@ -2,102 +2,97 @@ import java.util.*;
 
 public class Main {
 
-	static int N;
-	static int M;
-	static int[] oilBank;
+    static int N;
+    static int M;
+    static int[] oilBank;
 
-	static List<Node>[] list;
-	static int[][] dist;
+    static List<Node>[] list;
+    static long[][] dist;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		N = sc.nextInt();
-		M = sc.nextInt();
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-		oilBank = new int[N + 1];
-		for (int i = 1; i < N + 1; i++) {
-			oilBank[i] = sc.nextInt();
-		}
+        oilBank = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            oilBank[i] = sc.nextInt();
+        }
 
-		list = new ArrayList[N + 1];
-		for (int i = 1; i < N + 1; i++) {
-			list[i] = new ArrayList();
-		}
+        list = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-		for (int i = 0; i < M; i++) {
-			int start = sc.nextInt();
-			int end = sc.nextInt();
-			int di = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int di = sc.nextInt();
 
-			list[start].add(new Node(end, di));
-			list[end].add(new Node(start, di));
-		}
+            list[start].add(new Node(end, di));
+            list[end].add(new Node(start, di));
+        }
 
-		dist = new int[N + 1][2501];
-		for (int i = 1; i < N + 1; i++) {
-			Arrays.fill(dist[i], Integer.MAX_VALUE);
-		}
-		dikjstra(1);
+        dist = new long[N + 1][2501];
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(dist[i], Long.MAX_VALUE);
+        }
 
-		int answer = Integer.MAX_VALUE;
-		for (int cnt : dist[N]) {
-			answer = Math.min(answer, cnt);
-		}
-		System.out.println(answer);
+        dijkstra(1);
 
-	}
+        long answer = Long.MAX_VALUE;
+        for (long cnt : dist[N]) {
+            answer = Math.min(answer, cnt);
+        }
+        System.out.println(answer);
+    }
 
-	public static void dikjstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue();
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0, oilBank[start]));
+        dist[start][oilBank[start]] = 0;
 
-		pq.offer(new Node(start, 0, oilBank[start]));
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.idx;
+            long oilSum = node.oilSum;
+            int min = node.min;
 
-		while (!pq.isEmpty()) {
+            if (oilSum > dist[now][min])
+                continue;
 
-			Node node = pq.poll();
-			int now = node.idx;
-			int oilSum = node.oilSum;
-			int min = node.min;
+            for (Node next : list[now]) {
+                long newOilSum = oilSum + (long) min * next.dist;
+                int newMin = Math.min(min, oilBank[next.idx]);
+                if (newOilSum < dist[next.idx][newMin]) {
+                    pq.offer(new Node(next.idx, newOilSum, newMin));
+                    dist[next.idx][newMin] = newOilSum;
+                }
+            }
+        }
+    }
 
-			if (oilSum > dist[now][min])
-				continue;
+    static class Node implements Comparable<Node> {
+        int idx;
+        long oilSum;
+        int dist;
+        int min;
 
-			for (Node next : list[now]) {
-				/**
-				 * min 값을 갱신한다. min 값에 도착한 cost를 추가한다. 이 cost 비용이 더 비쌀 경우 스킵하고 더 낮을 경우 pq에 넣는다.
-				 */
-				int newOilSum = oilSum + (min * next.dist);
-				int newMin = Math.min(min, oilBank[next.idx]);
-				if (newOilSum < dist[next.idx][newMin]) {
-					pq.offer(new Node(next.idx, newOilSum, newMin));
-					dist[next.idx][newMin] = newOilSum;
-				}
+        public Node(int idx, int dist) {
+            this.idx = idx;
+            this.dist = dist;
+        }
 
-			}
-		}
+        public Node(int idx, long oilSum, int min) {
+            this.idx = idx;
+            this.oilSum = oilSum;
+            this.min = min;
+        }
 
-	}
-
-	static class Node implements Comparable<Node> {
-		int idx;
-		int oilSum;
-		int dist;
-		int min;
-
-		public Node(int idx, int dist) {
-			this.idx = idx;
-			this.dist = dist;
-		}
-
-		public Node(int idx, int oilSum, int min) {
-			this.idx = idx;
-			this.oilSum = oilSum;
-			this.min = min;
-		}
-
-		public int compareTo(Node o) {
-			return Integer.compare(this.oilSum, o.oilSum);
-		}
-	}
+        @Override
+        public int compareTo(Node o) {
+            return Long.compare(this.oilSum, o.oilSum);
+        }
+    }
 }
